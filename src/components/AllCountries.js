68d930d4b70cb9df96ea1c08161f10b0dd34a7bf
mamatha <mamathagowdg@gmail.com>
2020-05-13 15:26:06 +0530

    updated

--- src/components/AllCountries.js
+++ src/components/AllCountries.js
@@ -3,30 +3,22 @@ import { Link } from "react-router-dom";
 import styles from "./AllCountries.css";
 
 const CountryData = (props) => {
-  const {
-    countrycode,
-    name,
-    flag,
-    population,
-    region,
-    capital,
-  } = props.country;
   return (
     <div className="pb-3 w-100">
-      <Link to={`/countries/${countrycode}`}>
-        <img className="mb-3 w-100 h-100" src={flag} />
-        <h2>{name}</h2>
+      <Link to={`/countries/${props.country.countrycode}`}>
+        <img className="mb-3 w-100 h-100" src={props.country.flag} />
+        <h2>{props.country.name}</h2>
         <p className="population">
           <span>Population:</span>
-          {population}
+          {props.country.population}
         </p>
         <p className="population">
           <span>Region:</span>
-          {region}
+          {props.country.region}
         </p>
         <p className="population">
           <span>Capital:</span>
-          {capital}
+          {props.country.capital}
         </p>
       </Link>
     </div>
